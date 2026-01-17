4e332b3997417b74442604d8604f590dfad15393
Nitish <your_github_email@example.com>
2026-01-17 14:24:36 +0530

    shopcontrollers

--- backend/controllers/shop.controllers.js
+++ backend/controllers/shop.controllers.js
@@ -1,3 +1,4 @@
+const Shop = require("../models/shop.model");
 const uploadOnCloudinary = require("../utils/cloudinary");
 
 
@@ -9,7 +10,22 @@ const creatteShop=async(req,res)=>{
         if(req.file){
             image=await uploadOnCloudinary(req.file.path)
         }
+        const shop=await Shop.create({
+            name,
+            city,
+            state,
+            address,
+            image,
+            owner:req.userId
+        })
+        await shop.populate("owner")
+     
+        return res.status(201).json(shop)
+
     } catch (error) {
-        
+        return res.status(500).json({message:`create shop error${error}`})
     }
 }
+
+
+const editShop=async()
